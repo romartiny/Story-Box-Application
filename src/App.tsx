f68a82c81a853c9827a240f8ef37eacacc640043
Romartiny <romortiny@gmail.com>
2023-10-05 17:11:51 +0300

    implement routes with lazy loading/chunking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom"
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense} from "react";
 
 const App = () => {
   return (
-    <div className="app">Start</div>
+    <div className="app">
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+          <Route path={'/about'} element={<AboutPageAsync/>}/>
+        </Routes>
+      </Suspense>
+    </div>
   );
 }
 
