0540adc89d2d6a694cd4df2b81e122ba1cb9dcb0
Romartiny <romortiny@gmail.com>
2023-10-05 21:06:34 +0300

    fix problems with pages and classnames

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
-import {Suspense} from "react";
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom"
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPageAsync} from "pages/AboutPage/ui/AboutPage.async";
+import {AppRouter} from "app/providers/router";
+import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import React, {Suspense} from "react";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -13,13 +14,15 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Theme</button>
       <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/'} element={<MainPage />}/>
-          <Route path={'/about'} element={<AboutPageAsync />}/>
-        </Routes>
-      </Suspense>
+      <Link to={'/about'}> About</Link>
+      {/*<Suspense fallback={<div>Loading...</div>}>*/}
+      {/*  <Routes>*/}
+      {/*    <Route path={'/about'} element={<AboutPage />} />*/}
+      {/*    <Route path={'/'} element={<MainPage />} />*/}
+      {/*  </Routes>*/}
+      {/*</Suspense>*/}
+
+      <AppRouter />
     </div>
   );
 }
